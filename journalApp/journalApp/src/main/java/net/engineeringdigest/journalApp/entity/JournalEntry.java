66f5e6e83ded837ea49b5b800ead418e995731d8
PRATHAMESH YUVRAJ PAWAR <49082581+prathamesh213@users.noreply.github.com>
2024-07-05 20:16:08 +0530

    Add files via upload

---
+++ journalApp/journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -0,0 +1,30 @@
+package net.engineeringdigest.journalApp.entity;
+
+
+import java.time.LocalDateTime;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.mongodb.lang.NonNull;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection="journal_entry")
+@Data
+@NoArgsConstructor
+public class JournalEntry {
+   
+    @Id
+    private ObjectId id;
+    @NonNull
+    private String tittle;
+     
+    private String content;
+
+    private LocalDateTime date;
+
+     
+}
